50a0be89f2bb88ce652857ad63b9f23a628099a3
KosenkoMarta <ingressjimenez@gmail.com>
2023-05-31 22:44:36 +0300

    Reworking tests Using a Data Provider

--- src/test/java/person/man/ManIsRetiredTest.java
+++ src/test/java/person/man/ManIsRetiredTest.java
@@ -2,30 +2,32 @@ package person.man;
 
 import com.rd.world.Man;
 import com.rd.world.Person;
-import com.rd.world.Woman;
+import dataProviders.TestDataProviderMan;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ManIsRetiredTest {
     private Man men;
     private Man menSecond;
 
-    @BeforeClass
-    public void addNewMenObject(){
-        Person partner = new Woman("Marta", "Kosenko", 59, men, "Kosenko");
-        Person partnerSecond = new Woman("Alina", "Lysenko", 50, menSecond, "maidenAlina");
-        men = new Man("Dmytro", "Shevchenko", 65, partner);
-        menSecond = new Man("Yaroslav", "Lysenko", 55, partnerSecond);
-    }
+//    @BeforeClass
+//    public void addNewMenObject(){
+//        Person partner = new Woman("Marta", "Kosenko", 59, men, "Kosenko");
+//        Person partnerSecond = new Woman("Alina", "Lysenko", 50, menSecond, "maidenAlina");
+//        men = new Man("Dmytro", "Shevchenko", 65, partner);
+//        menSecond = new Man("Yaroslav", "Lysenko", 55, partnerSecond);
+//    }
     //test is retired
-    @Test
-    public void testIsRetired(){
+    @Test(dataProvider = "menAfterRetired", dataProviderClass = TestDataProviderMan.class, groups = {"TestIsRetired"})
+    public void testIsRetired(String firstName, String lastName, int age, Person partner){
+        men = new Man(firstName, lastName, age, partner);
         Assert.assertTrue(men.isRetired(), "Невірний результат перевірки");
     }
+
     //test is not retired
-    @Test
-    public void testIsNotRetired(){
+    @Test(dataProvider = "menBeforeRetired", dataProviderClass = TestDataProviderMan.class, groups = {"TestIsNotRetired"})
+    public void testIsNotRetired(String firstName, String lastName, int age, Person partner){
+        menSecond = new Man(firstName, lastName, age, partner);
         Assert.assertFalse(menSecond.isRetired(), "Невірний результат перевірки");
     }
 }
--- src/test/java/person/woman/WomanIsRetiredTest.java
+++ src/test/java/person/woman/WomanIsRetiredTest.java
@@ -1,31 +1,32 @@
 package person.woman;
 
-import com.rd.world.Man;
 import com.rd.world.Person;
 import com.rd.world.Woman;
+import dataProviders.TestDataProviderWoman;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class WomanIsRetiredTest {
     private Woman women;
     private Woman womenSecond;
 
-    @BeforeClass
-    public void addNewMenObject(){
-        Person partner = new Man("Dmytro", "Shevchenko", 65, women);
-        Person partnerSecond = new Man("Yaroslav", "Lysenko", 55, womenSecond);
-        women = new Woman("Marta", "Kosenko", 62, partner, "Kosenko");
-        womenSecond = new Woman("Alina", "Lysenko", 50, partnerSecond, "maidenAlina");
-    }
+//    @BeforeClass
+//    public void addNewMenObject(){
+//        Person partner = new Man("Dmytro", "Shevchenko", 65, women);
+//        Person partnerSecond = new Man("Yaroslav", "Lysenko", 55, womenSecond);
+//        women = new Woman("Marta", "Kosenko", 62, partner, "Kosenko");
+//        womenSecond = new Woman("Alina", "Lysenko", 50, partnerSecond, "maidenAlina");
+//    }
     //test is retired
-    @Test
-    public void testIsRetired(){
+    @Test(dataProvider = "womenAfterRetired", dataProviderClass = TestDataProviderWoman.class, groups = {"TestIsRetired"})
+    public void testIsRetired(String firstName, String lastName, int age, Person partner, String maidenName){
+        women = new Woman(firstName, lastName, age, partner, maidenName);
         Assert.assertTrue(women.isRetired(), "Невірний результат перевірки");
     }
     //test is not retired
-    @Test
-    public void testIsNotRetired(){
+    @Test(dataProvider = "womenBeforeRetired", dataProviderClass = TestDataProviderWoman.class, groups = {"TestIsNotRetired"})
+    public void testIsNotRetired(String firstName, String lastName, int age, Person partner, String maidenName){
+        womenSecond = new Woman(firstName, lastName, age, partner, maidenName);
         Assert.assertFalse(womenSecond.isRetired(), "Невірний результат перевірки");
     }
 }
